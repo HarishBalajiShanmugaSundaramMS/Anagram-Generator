ae3f5a352fef40ce24c632ccfa76689f9511edaa
HarishBalaji ShanmugaSundaram <harishbalajishanmugasundaram@gmail.com>
2019-09-20 07:35:41 +0200

    Multi-Language Added.

--- AnagramGenerator.py
+++ AnagramGenerator.py
@@ -2,8 +2,9 @@ import timeit
 import tkinter as tk
 from itertools import permutations
 from tkinter import *
-import enchant
 from tkinter import messagebox
+
+import enchant
 
 root = tk.Tk()
 root.title('Anagram Generator')
@@ -20,6 +21,8 @@ root.config(bg='#25D366')
 # ?==========================================================================
 
 # ?==========Function Definitions Begins Here================================
+
+
 def clearText():
     text01.configure(state='normal')
     text01.delete('1.0', END)
@@ -36,7 +39,8 @@ def generateAnagram():
     if len(entry01.get()) == 0:
         msg = messagebox.showwarning('Empty Input', 'Please Enter A Word')
     else:
-        d = enchant.Dict('en_US')
+        ss=var.get()
+        d = enchant.Dict(ss)
         word = str(entry01.get())
         word = word.upper()
         L = list(word)
@@ -64,15 +68,23 @@ entry01.focus()  # * Sets Focus
 entry01.bind('<Return>', onReturn)
 button01 = tk.Button(root, text='Generate', command=generateAnagram)
 button02 = tk.Button(root, text='Clear', command=clearText)
-text01 = tk.Text(root, font=('calibri', 20, 'bold'),state='disabled')
+text01 = tk.Text(root, font=('calibri', 20, 'bold'), state='disabled')
 text01.config(width=20, height=10, bg='aqua')
 
-# ?==============Widget Arrangement in Grid==================================
-label01.grid(row=0, column=0, padx=5, pady=5, sticky=W+E+N+S)
-entry01.grid(row=1, column=0, padx=5, pady=5, sticky=W+E+N+S)
-button01.grid(row=2, column=0, padx=5, pady=5, sticky=W+E+N+S)
-text01.grid(row=3, column=0, padx=5, pady=5, sticky=W+E+N+S)
-button02.grid(row=4, column=0, padx=5, pady=5, sticky=W+E+N+S)
-# ?==========================================================================
+#optionlist01=['English-US','English-UK','English-AU','German','French']
+optionlist01=['de-DE','fr-FR','en-GB','en-AU','en-US']
+var=tk.StringVar(root)
+var.set(optionlist01[1])
+optionmenu01=tk.OptionMenu(root,var,*optionlist01)
+optionmenu01.config(bg='#FCE762')
 
+# ?==============Widget Arrangement in Grid==================================
+optionmenu01.grid(row=0, column=0, padx=5, pady=5, sticky=W+E+N+S)
+label01.grid(row=1, column=0, padx=5, pady=5, sticky=W+E+N+S)
+entry01.grid(row=2, column=0, padx=5, pady=5, sticky=W+E+N+S)
+button01.grid(row=3, column=0, padx=5, pady=5, sticky=W+E+N+S)
+text01.grid(row=4, column=0, padx=5, pady=5, sticky=W+E+N+S)
+button02.grid(row=5, column=0, padx=5, pady=5, sticky=W+E+N+S)
+# ?==========================================================================
+#print(enchant.list_languages())
 root.mainloop()
